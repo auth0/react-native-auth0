cb73b4106c1231ea70ff89c9d30ddfa890abd74c
Danilo Figueiredo <grifotv@googlemail.com>
2017-06-04 22:49:22 +0100

    Fix typos in doc of what each parameter (#23)

--- management/api.js
+++ management/api.js
@@ -28,8 +28,8 @@ export default class UsersAPI {
 
   link(id, otherUserToken) {
     return Promise.all([
-      nonNull(id, 'must supply an identifier of the user to to link to'),
-      nonNull(otherUserToken, 'must supply a valid token of the user to to link')
+      nonNull(id, 'must supply an identifier of the user to link to'),
+      nonNull(otherUserToken, 'must supply a valid token of the user to link')
     ]).then(([id, otherUserToken]) => {
       return json('POST', `${this.baseUrl}/api/v2/users/${encodeURIComponent(id)}/identities`, {'link_with': otherUserToken}, {'Authorization': `Bearer ${this.token}`});
     });
@@ -37,7 +37,7 @@ export default class UsersAPI {
 
   unlink(id, identityId, provider) {
     return Promise.all([
-      nonNull(id, 'must provider a user identifier from where the identity will be unlinked'),
+      nonNull(id, 'must provider an user identifier from where the identity will be unlinked'),
       nonNull(identityId, 'must supply an identity identifier to be unlinked'),
       nonNull(provider, 'must supply a provider name of the identity to be unlinked')
     ]).then(([id, identityId, provider]) => {
