be653136f18b36ab4eba5a18dd9a586da87c3717
Jim Remsik <jremsikjr@gmail.com>
2017-06-04 16:49:01 -0500

    Guard against unavailable captureStackTrace (#33)

--- utils/error.js
+++ utils/error.js
@@ -1,7 +1,9 @@
 export default class Auth0Error extends Error {
   constructor (json) {
     super();
-    Error.captureStackTrace(this, this.constructor);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
     this.name = json.error || json.code || 'a0.internal.failed';
     this.message = json.description || json.error_description;
     this.json = json;
