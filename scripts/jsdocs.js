ea6119bcadb3e3e1580d89ff5ed0f9c310d7771a
Hernan Zalazar <hernan.zalazar@gmail.com>
2017-06-15 01:28:04 -0300

    Improve release scripts

--- scripts/jsdocs.js
+++ scripts/jsdocs.js
@@ -1,6 +1,6 @@
 if (process.platform === 'win32') {
   console.error('Must be run on a Unix OS');
-  exit(1);
+  process.exit(1);
 }
 
 var library = require('../package.json');
@@ -12,3 +12,4 @@ if (fs.existsSync('docs')) {
   execSync('rm -r docs', {stdio: 'inherit'});
 }
 execSync(`mv out/react-native-auth0/${library.version}/ docs`, {stdio: 'inherit'});
+execSync('git add docs');
--- scripts/telemetry.js
+++ scripts/telemetry.js
@@ -6,5 +6,7 @@ if (process.platform === 'win32') {
 var library = require('../package.json');
 var fs = require('fs');
 var telemetry = require('../networking/telemetry');
+var execSync = require('child_process').execSync;
 telemetry.version = library.version;
 fs.writeFileSync('networking/telemetry.js', `// Generated file by ${process.env['USER']} on ${new Date()};\nmodule.exports = ${JSON.stringify(telemetry, null, 2)};`);
+execSync('git add networking/telemetry.js');
