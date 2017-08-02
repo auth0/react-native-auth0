b53b528852188772b4a5625f5f39a8ce05790ff4
Luke Williams <lukecwilliams@users.noreply.github.com>
2017-08-02 16:16:20 -0400

    Update A0Auth0Package.java

--- android/src/main/java/com/auth0/react/A0Auth0Package.java
+++ android/src/main/java/com/auth0/react/A0Auth0Package.java
@@ -18,7 +18,7 @@ public class A0Auth0Package implements ReactPackage {
 
     // Required @Override for RN v0.46.x and earlier
     public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
+      return Collections.emptyList();
     }
 
     @Override
