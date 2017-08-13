cfe16c3483db6ad4eb4a424e14ba469a3a8e08da
Luke Williams <lukecwilliams@users.noreply.github.com>
2017-08-13 15:01:29 -0400

    Urgent: Remove breaking createJSModules override (#70)
    Remove override annotation on createJSModules method

--- android/src/main/java/com/auth0/react/A0Auth0Package.java
+++ android/src/main/java/com/auth0/react/A0Auth0Package.java
@@ -16,7 +16,7 @@ public class A0Auth0Package implements ReactPackage {
       return Arrays.<NativeModule>asList(new A0Auth0Module(reactContext));
     }
 
-    @Override
+    // Required @Override for RN v0.46.x and earlier
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
