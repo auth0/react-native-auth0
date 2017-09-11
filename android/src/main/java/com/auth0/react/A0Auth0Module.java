b70b1999e48d7eb615e38758f231ffc58c524d66
Martin Walsh <martin.walsh@gmail.com>
2017-09-11 12:38:30 +0100

    Close Browser app tab after finishing Auth (#90)

--- android/src/main/java/com/auth0/react/A0Auth0Module.java
+++ android/src/main/java/com/auth0/react/A0Auth0Module.java
@@ -58,9 +58,11 @@ public class A0Auth0Module extends ReactContextBaseJavaModule implements Lifecyc
         if (activity != null) {
             CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
             CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
             customTabsIntent.launchUrl(activity, Uri.parse(url));
         } else {
             final Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
             intent.setData(Uri.parse(url));
             getReactApplicationContext().startActivity(intent);
         }
