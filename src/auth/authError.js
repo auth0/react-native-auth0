a6d7ff2512d674f3173262bf3ebc75975c8d516f
Emilio <emiliokyp@gmail.com>
2020-03-06 13:58:33 +1100

    handle invalid token error

--- src/auth/authError.js
+++ src/auth/authError.js
@@ -2,13 +2,18 @@ import BaseError from '../utils/baseError';
 
 export default class AuthError extends BaseError {
   constructor(response) {
-    const { status, json = {}, text } = response;
-    const { error, error_description: description } = json;
+    const {status, json = {}, text} = response;
+    const {error, error_description: description} = json;
     super(
       error || 'a0.response.invalid',
-      description || text || 'unknown error'
+      description || text || handleInvalidToken(response) || 'unknown error',
     );
     this.json = json;
     this.status = status;
   }
 }
+
+const handleInvalidToken = response =>
+  response?.headers?.map['www-authenticate'].match(/error="invalid_token"/g)
+    ? 'invalid_token'
+    : null;
