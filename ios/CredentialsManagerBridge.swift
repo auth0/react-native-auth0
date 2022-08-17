92ab9db01a819196c942b38615f624e3f2a2de77
Poovamraj T T <poovamraj@gmail.com>
2022-08-17 16:36:15 +0200

    Swift code cleanup

--- ios/CredentialsManagerBridge.swift
+++ ios/CredentialsManagerBridge.swift
@@ -6,6 +6,7 @@
 //
 
 import Auth0
+import Foundation
 
 @objc
 public class CredentialsManagerBridge: NSObject {
@@ -25,23 +26,48 @@ public class CredentialsManagerBridge: NSObject {
     
     var credentialsManager: CredentialsManager
     
-    @objc public init(clientId: NSString, domain: NSString) {
+    @objc public init(clientId: String, domain: String) {
         let auth0 = Auth0
-            .authentication(clientId: clientId as String, domain: domain as String)
+            .authentication(clientId: clientId, domain: domain)
         self.credentialsManager = CredentialsManager(authentication: auth0)
         super.init()
    }
     
-    @objc public func saveCredentials(credentialsMap: NSDictionary, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        let credentials = Credentials.fromNSDictionary(credentialsMap: credentialsMap)
+    @objc public func saveCredentials(credentialsDict: [String: Any], resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+        
+        guard let accessToken = credentialsDict[CredentialsManagerBridge.accessTokenKey] as? String, let tokenType = credentialsDict[CredentialsManagerBridge.tokenTypeKey] as? String, let idToken = credentialsDict[CredentialsManagerBridge.idTokenKey] as? String else { reject(CredentialsManagerBridge.errorCode, "Incomplete information provided for credentials", NSError.init(domain: CredentialsManagerBridge.errorCode, code: -99999, userInfo: nil)); return; }
+        
+        let refreshToken = credentialsDict[CredentialsManagerBridge.refreshTokenKey] as? String
+        let scope = credentialsDict[CredentialsManagerBridge.scopeKey] as? String
+        var expiresIn: Date?
+         if let string = credentialsDict[CredentialsManagerBridge.expiresInKey] as? String, let double = Double(string) {
+             expiresIn = Date(timeIntervalSinceNow: double)
+         } else if let double = credentialsDict[CredentialsManagerBridge.expiresInKey] as? Double {
+             expiresIn = Date(timeIntervalSinceNow: double)
+         } else if let dateStr = credentialsDict[CredentialsManagerBridge.expiresInKey] as? String {
+             let dateFormatter = DateFormatter()
+             dateFormatter.dateFormat = CredentialsManagerBridge.dateFormat
+             expiresIn = dateFormatter.date(from: dateStr)
+         }
+     
+        
+        let credentials =  Credentials(
+            accessToken: accessToken,
+            tokenType: tokenType,
+            idToken:  idToken,
+            refreshToken: refreshToken,
+            expiresIn: expiresIn ?? Date(),
+            scope: scope,
+            recoveryCode: nil
+        )
         resolve(credentialsManager.store(credentials: credentials))
     }
     
-    @objc public func getCredentials(scope: NSString?, minTTL: NSInteger, parameters: NSDictionary?, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        credentialsManager.credentials(withScope: scope as String?, minTTL: minTTL, parameters: parameters as! [String : Any]) { result in
+    @objc public func getCredentials(scope: String?, minTTL: Int, parameters: [String: Any], resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        credentialsManager.credentials(withScope: scope, minTTL: minTTL, parameters: parameters) { result in
             switch result {
             case .success(let credentials):
-                resolve(credentials.toNSDictionary())
+                resolve(credentials.asDictionary())
             case .failure(let error):
                 reject(CredentialsManagerBridge.errorCode, error.errorDescription, error)
             }
@@ -49,58 +75,29 @@ public class CredentialsManagerBridge: NSObject {
     }
     
     @objc public func hasValidCredentials(minTTL: Int, resolve: RCTPromiseResolveBlock) {
-        resolve(credentialsManager.hasValid(minTTL: minTTL))
+        resolve(credentialsManager.canRenew() || credentialsManager.hasValid(minTTL: minTTL))
     }
     
     @objc public func clearCredentials(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
         resolve(credentialsManager.clear())
     }
     
-    @objc public func enableLocalAuthentication(title: NSString?, cancelTitle: NSString?, fallbackTitle: NSString?) {
-        let titleValue: NSString = title ?? "Please authenticate to continue"
-        self.credentialsManager.enableBiometrics(withTitle: titleValue as String, cancelTitle: cancelTitle as String?, fallbackTitle: fallbackTitle as String?)
+    @objc public func enableLocalAuthentication(title: String?, cancelTitle: String?, fallbackTitle: String?) {
+        let titleValue = title ?? "Please authenticate to continue"
+        self.credentialsManager.enableBiometrics(withTitle: titleValue, cancelTitle: cancelTitle, fallbackTitle: fallbackTitle)
     }
 }
 
 
 extension Credentials {
-    static func fromNSDictionary(credentialsMap: NSDictionary) -> Credentials {
-        let accessToken = credentialsMap[CredentialsManagerBridge.accessTokenKey]
-        let idToken = credentialsMap[CredentialsManagerBridge.idTokenKey]
-        let tokenType = credentialsMap[CredentialsManagerBridge.tokenTypeKey]
-        let refreshToken = credentialsMap[CredentialsManagerBridge.refreshTokenKey] as? String
-        let scope = credentialsMap[CredentialsManagerBridge.scopeKey] as? String
-        
-        var expiresIn: Date?
-        if let string = credentialsMap[CredentialsManagerBridge.expiresInKey] as? String, let double = Double(string) {
-            expiresIn = Date(timeIntervalSinceNow: double)
-        } else if let double = credentialsMap[CredentialsManagerBridge.expiresInKey] as? Double {
-            expiresIn = Date(timeIntervalSinceNow: double)
-        } else if let dateStr = credentialsMap[CredentialsManagerBridge.expiresInKey] as? String {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = CredentialsManagerBridge.dateFormat
-            expiresIn = dateFormatter.date(from: dateStr)
-        }
-        
-        return Credentials(
-            accessToken: accessToken as! String,
-            tokenType: tokenType as! String,
-            idToken: idToken as! String,
-            refreshToken: refreshToken,
-            expiresIn: expiresIn ?? Date(),
-            scope: scope,
-            recoveryCode: nil
-        )
-    }
-    
-    func toNSDictionary() -> NSDictionary {
-        return NSDictionary(dictionary: [
+    func asDictionary() -> [String: Any] {
+        return [
             CredentialsManagerBridge.accessTokenKey: self.accessToken,
             CredentialsManagerBridge.tokenTypeKey: self.tokenType,
             CredentialsManagerBridge.idTokenKey: self.idToken,
-            CredentialsManagerBridge.refreshTokenKey: self.refreshToken,
+            CredentialsManagerBridge.refreshTokenKey: self.refreshToken as Any,
             CredentialsManagerBridge.expiresInKey: self.expiresIn,
-            CredentialsManagerBridge.scopeKey: self.scope
-        ])
+            CredentialsManagerBridge.scopeKey: self.scope as Any
+        ]
     }
 }
