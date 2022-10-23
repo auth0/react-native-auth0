cc685a4ccd52f7ff8f9a709b6679ad6582c07556
filose <jfilose@gmail.com>
2022-10-23 13:15:04 +0100

    Revert whitespace changes

--- src/webauth/__mocks__/react-native.js
+++ src/webauth/__mocks__/react-native.js
@@ -4,7 +4,7 @@ import A0Auth0 from './auth0';
 const mock = {};
 
 mock.Linking = new Linking();
-mock.NativeModules = {A0Auth0: new A0Auth0()};
-mock.Platform = {OS: 'test-os'};
+mock.NativeModules = { A0Auth0: new A0Auth0() };
+mock.Platform = { OS: 'test-os' };
 
 module.exports = mock;
