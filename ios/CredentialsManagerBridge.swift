cee5589dc879a5dad336b3353b2e6f287112fbdc
Andy Stanberry <andystanberry@gmail.com>
2022-11-11 17:04:54 +0000

    Return a unix timestamp for expiresIn

--- ios/CredentialsManagerBridge.swift
+++ ios/CredentialsManagerBridge.swift
@@ -96,7 +96,7 @@ extension Credentials {
             CredentialsManagerBridge.tokenTypeKey: self.tokenType,
             CredentialsManagerBridge.idTokenKey: self.idToken,
             CredentialsManagerBridge.refreshTokenKey: self.refreshToken as Any,
-            CredentialsManagerBridge.expiresInKey: self.expiresIn,
+            CredentialsManagerBridge.expiresInKey: self.expiresIn.timeIntervalSince1970,
             CredentialsManagerBridge.scopeKey: self.scope as Any
         ]
     }
