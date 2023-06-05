48b8af995aaa4fbb8ee4fdb78cfa4b3f29879163
Poovamraj T T <poovamraj@gmail.com>
2023-06-05 19:43:01 +0530

    Simplify user conversion

--- src/utils/userConversion.ts
+++ src/utils/userConversion.ts
@@ -1,60 +1,52 @@
-import {JwtPayload} from 'jwt-decode';
 import {RawUser} from '../internal-types';
-import {User} from '../types';
-import {idTokenNonProfileClaims} from '../jwt/utils';
 
-export function convertUser(payload: JwtPayload): User {
-  return Object.keys(payload).reduce((profile: RawUser, claim) => {
-    if (!idTokenNonProfileClaims.has(claim)) {
-      profile[claim] = payload[claim];
-    }
-    const user: User = {
-      name: profile.name,
-      givenName: profile.given_name,
-      familyName: profile.family_name,
-      middleName: profile.middle_name,
-      nickname: profile.nickname,
-      preferredUsername: profile.preferred_username,
-      profile: profile.profile,
-      picture: profile.picture,
-      website: profile.website,
-      email: profile.email,
-      emailVerified: profile.email_verified,
-      gender: profile.gender,
-      birthdate: profile.birthdate,
-      zoneinfo: profile.zoneinfo,
-      locale: profile.locale,
-      phoneNumber: profile.phone_number,
-      phoneNumberVerified: profile.phone_number_verified,
-      address: profile.address,
-      updatedAt: profile.updated_at,
-      sub: profile.sub,
-    };
-    delete profile.name;
-    delete profile.given_name;
-    delete profile.family_name;
-    delete profile.middle_name;
-    delete profile.nickname;
-    delete profile.preferred_username;
-    delete profile.profile;
-    delete profile.picture;
-    delete profile.website;
-    delete profile.email;
-    delete profile.email_verified;
-    delete profile.gender;
-    delete profile.birthdate;
-    delete profile.zoneinfo;
-    delete profile.locale;
-    delete profile.phone_number;
-    delete profile.phone_number_verified;
-    delete profile.address;
-    delete profile.updated_at;
-    delete profile.sub;
-    Object.keys(user).forEach(key => {
-      if (user[key] === undefined) {
-        delete user[key];
+function snakeToCamel(str: string): string {
+  var parts = str.split('_');
+  return (
+    parts.reduce(function(p, c) {
+      return p + c.charAt(0).toUpperCase() + c.slice(1);
+    }, parts.shift()) ?? ''
+  );
+}
+
+export function convertUser(payload: RawUser) {
+  const claimsToCamelize: Array<keyof RawUser> = [
+    'name',
+    'given_name',
+    'family_name',
+    'middle_name',
+    'nickname',
+    'preferred_username',
+    'profile',
+    'picture',
+    'website',
+    'email',
+    'email_verified',
+    'gender',
+    'birthdate',
+    'zoneinfo',
+    'locale',
+    'phone_number',
+    'phone_number_verified',
+    'address',
+    'updated_at',
+    'sub',
+  ];
+
+  return Object.keys(payload).reduce(
+    (profile: Partial<RawUser>, claim: string) => {
+      if ((claimsToCamelize as string[]).includes(claim)) {
+        return {
+          ...profile,
+          [snakeToCamel(claim)]: payload[claim],
+        };
+      } else {
+        return {
+          ...profile,
+          [claim]: payload[claim],
+        };
       }
-    });
-    return {...user, ...profile};
-  }, {});
+    },
+    {},
+  );
 }
