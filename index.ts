f61798c25897415809264c1c63f2d306c047d8a3
poovamraj <poovamraj@gmail.com>
2023-07-08 09:44:38 +0200

    Fix expo plugin export

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { TimeoutError } from './src/utils/fetchWithTimeout';
-export { default as useAuth0 } from './src/hooks/use-auth0';
-export { default as Auth0Provider } from './src/hooks/auth0-provider';
-export { default as LocalAuthenticationStrategy } from './src/credentials-manager/localAuthenticationStrategy';
-
-import Auth0 from './src/auth0';
-export default Auth0;
