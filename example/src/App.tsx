c5bb94e57d2a9fb4c45ea2266cf11e64655f35df
Sai Venkat Desu <venkat.desu@okta.com>
2024-07-19 12:52:02 +0530

    chore: updated sample app to contain mutliple screens and each screen using different Auth0Provider
    Signed-off-by: Sai Venkat Desu <venkat.desu@okta.com>

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -10,8 +10,53 @@ import React from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import { useAuth0, Auth0Provider } from 'react-native-auth0';
 import config from './auth0-configuration';
+import { NavigationProp, NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Home = () => {
+const Stack = createNativeStackNavigator();
+
+const Home = ({ navigation }: { navigation: NavigationProp<any> }) => {
+  const { authorize, clearSession, user, getCredentials, error } = useAuth0();
+
+  const onLogin = async () => {
+    await authorize();
+    const credentials = await getCredentials(undefined, 0, {});
+    Alert.alert('AccessToken: ' + credentials?.accessToken);
+  };
+
+  const loggedIn = user !== undefined && user !== null;
+
+  const onLogout = async () => {
+    await clearSession();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}> Auth0Sample - Login </Text>
+      {user && <Text>You are logged in as {user.name}</Text>}
+      {!user && <Text>You are not logged in</Text>}
+      <Button
+        onPress={loggedIn ? onLogout : onLogin}
+        title={loggedIn ? 'Log Out' : 'Log In'}
+      />
+      <Button
+        title="Go to Second Screen"
+        onPress={() => navigation.navigate('Second')}
+      />
+      {error && <Text style={styles.error}>{error.message}</Text>}
+    </View>
+  );
+};
+
+const HomeProvider = ({ navigation }: { navigation: NavigationProp<any> }) => {
+  return (
+    <Auth0Provider domain={config.domain} clientId={config.clientId}>
+      <Home navigation={navigation} />
+    </Auth0Provider>
+  );
+};
+
+const SecondScreen = () => {
   const { authorize, clearSession, user, getCredentials, error } = useAuth0();
 
   const onLogin = async () => {
@@ -40,11 +85,25 @@ const Home = () => {
   );
 };
 
+const SecondScreenProvider = () => {
+  return (
+    <Auth0Provider
+      domain="venkat-desu.us.auth0.com"
+      clientId="Wy9qORYVTokJFuJsvbXa2JJ6dDQhBGzI"
+    >
+      <SecondScreen />
+    </Auth0Provider>
+  );
+};
+
 const App = () => {
   return (
-    <Auth0Provider domain={config.domain} clientId={config.clientId}>
-      <Home />
-    </Auth0Provider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeProvider} />
+        <Stack.Screen name="Second" component={SecondScreenProvider} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
