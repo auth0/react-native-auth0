6baf98bb318ce7a9a7413625b7c87b0cd3559fd3
Subhankar Maiti <35273200+subhankarmaiti@users.noreply.github.com>
2025-10-31 16:14:58 +0530

    doc: add DPoP error to the API doc (#1371)

--- src/exports/classes.ts
+++ src/exports/classes.ts
@@ -2,6 +2,7 @@ export {
   AuthError,
   CredentialsManagerError,
   WebAuthError,
+  DPoPError,
 } from '../core/models';
 export { default as Auth0 } from '../Auth0';
 export { TimeoutError } from '../core/utils/fetchWithTimeout';
