a02800f430c9a7249f17062268f65e37682490c2
Subhankar Maiti <subhankar.maiti@okta.com>
2026-05-11 10:42:41 +0530

    feat: add DPoP credential state error handling and update dependencies

--- ios/NativeBridge.swift
+++ ios/NativeBridge.swift
@@ -518,6 +518,9 @@ extension CredentialsManagerError {
                 code = "REVOKE_FAILED"
             } 
             case CredentialsManagerError.largeMinTTL: code = "LARGE_MIN_TTL"
+            case CredentialsManagerError.dpopKeyMissing: code = "DPOP_KEY_MISSING"
+            case CredentialsManagerError.dpopKeyMismatch: code = "DPOP_KEY_MISMATCH"
+            case CredentialsManagerError.dpopNotConfigured: code = "DPOP_NOT_CONFIGURED"
             default: code = "UNKNOWN"
         }
         return code
